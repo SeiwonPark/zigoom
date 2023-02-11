67e2d67cec7eaf0de40a2f02dd529d43c5c5a18b
SeiwonPark <psw7347@gmail.com>
2023-02-11 17:24:30 +0900

    feat: socket base connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,24 +1,36 @@
 import * as express from "express";
 import * as cors from "cors";
+import * as http from "http";
+import { Server, Socket } from "socket.io";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
 app.on("error", (err) => {
   console.log(err);
 });
 
-app
-  .get("/", (req: express.Request, res: express.Response) => {
-    res.send({ title: "GET Request", content: "Test" });
-  })
-  .post("/", (req, res) => {
-    res.send(req.body);
-  });
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000", // client url
+  },
+});
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+io.on("connection", (socket: Socket) => {
+  console.log("client connected: ", socket.id);
+  socket.join("clock-room");
+  socket.on("disconnect", (reason) => {
+    console.log(reason);
+  });
+});
+
+setInterval(() => {
+  io.to("clock-room").emit("time", new Date());
+}, 1000);
+
+httpServer.listen(PORT, () => {
+  console.log("Server running on Port ", PORT);
 });
