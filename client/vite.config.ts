4804bbdae9925b04a359079f1cc43c2ae0fb1067
SeiwonPark <psw7347@gmail.com>
2023-09-09 22:25:13 +0900

    feat: configure emotion css

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      plugins: [['@swc/plugin-emotion', {}]],
+    }),
+  ],
 })
