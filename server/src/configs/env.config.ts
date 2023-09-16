5b3007d5bde81e0c7ba15fa462730bf8b21a249a
SeiwonPark <psw7347@gmail.com>
2023-09-17 08:09:53 +0900

    fix: host

--- server/src/configs/env.config.ts
+++ server/src/configs/env.config.ts
@@ -6,5 +6,5 @@ const isDevelopment = process.env.NODE_ENV === 'development'
 
 export const PORT = isDevelopment ? 5001 : process.env.PORT || 5001
 export const ALLOWED_ORIGIN = isDevelopment
-  ? 'http://127.0.0.1:5173'
-  : process.env.ALLOWED_ORIGIN || 'http://127.0.0.1:5173'
+  ? 'http://localhost:5173'
+  : process.env.ALLOWED_ORIGIN || 'http://localhost:5173'
