fce35026d1d2f4295af9a383eeba3b42f8eb3fbc
SeiwonPark <psw7347@gmail.com>
2023-09-17 09:22:26 +0900

    fix: rename env

--- client/src/configs/env.ts
+++ client/src/configs/env.ts
@@ -1,3 +1,3 @@
 export const VITE_GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
-export const VITE_REDIRECT_URL = import.meta.env.VITE_REDIRECT_URL
+export const VITE_LOGIN_URL = import.meta.env.VITE_LOGIN_URL
