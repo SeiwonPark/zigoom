c55d8a278439386d32802c7014a5d39a6f6acc9d
SeiwonPark <psw7347@gmail.com>
2023-10-08 00:14:26 +0900

    fix(test): mock redis

--- server/src/__tests__/unit/controllers/auth.middleware.test.ts
+++ server/src/__tests__/unit/controllers/auth.middleware.test.ts
@@ -1,11 +1,29 @@
 import type { Request, Response, NextFunction } from 'express'
 import { authHandler, requireAuthentication } from '../../../shared/infra/http/middlewares/handlers'
 import { CustomError, ErrorCode } from '@shared/errors'
+import { decodeToken } from '@utils/token'
 
 jest.mock('@utils/token')
+jest.mock('@configs/redis.config', () => ({
+  redisClient: {
+    connect: jest.fn(),
+    set: jest.fn(),
+  },
+}))
+
+const validToken = {
+  exp: Date.now() / 1000 + 60,
+}
+const expiredToken = {
+  exp: Date.now() / 1000 - 60,
+}
 
 describe('Auth Middleware Unit Tests', () => {
   let req: Request, res: Response, next: NextFunction
+
+  const mockDecodeToken = (value: any): void => {
+    ;(decodeToken as jest.MockedFunction<typeof decodeToken>).mockResolvedValue(value)
+  }
 
   beforeEach(() => {
     req = {
@@ -34,8 +52,7 @@ describe('Auth Middleware Unit Tests', () => {
     test('should throw error when decodeToken returns null', async () => {
       expect.assertions(2)
 
-      require('@utils/token').decodeToken = jest.fn().mockResolvedValue(null)
-
+      mockDecodeToken(null)
       await expect(authHandler(req, res, next)).rejects.toEqual(
         new CustomError('Failed to get payload from token', ErrorCode.Unauthorized),
       )
@@ -45,8 +62,7 @@ describe('Auth Middleware Unit Tests', () => {
     test('should throw error when token is expired', async () => {
       expect.assertions(2)
 
-      require('@utils/token').decodeToken = jest.fn().mockResolvedValue({ exp: Date.now() / 1000 - 60 })
-
+      mockDecodeToken(expiredToken)
       await expect(authHandler(req, res, next)).rejects.toEqual(
         new CustomError('The token has been expired', ErrorCode.Unauthorized),
       )
@@ -56,7 +72,7 @@ describe('Auth Middleware Unit Tests', () => {
     test('should call next() with decoded token when token is valid', async () => {
       expect.assertions(2)
 
-      require('@utils/token').decodeToken = jest.fn().mockResolvedValue({ exp: Date.now() / 1000 + 60 })
+      mockDecodeToken(validToken)
       await authHandler(req, res, next)
 
       expect(req.ctx.user.isGuest).toBe(false)
