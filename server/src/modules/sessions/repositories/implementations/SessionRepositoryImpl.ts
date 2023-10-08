1309c1bfc78e8464db4907370143503fc04db991
SeiwonPark <psw7347@gmail.com>
2023-10-08 18:23:04 +0900

    fix: overload repository methods

--- server/src/modules/sessions/repositories/implementations/SessionRepositoryImpl.ts
+++ server/src/modules/sessions/repositories/implementations/SessionRepositoryImpl.ts
@@ -3,14 +3,20 @@ import { mysql } from '@configs/prisma.config'
 import SessionRepository, { JoinedSession } from '../SessionRepository'
 
 export default class SessionRepositoryImpl implements SessionRepository {
+  public async save(data: Prisma.SessionCreateInput): Promise<Session>
+  public async save(data: Prisma.SessionCreateInput, include: true): Promise<JoinedSession>
   public async save(data: Prisma.SessionCreateInput, include?: boolean): Promise<Session | JoinedSession> {
     return await mysql.session.create({ data: data, include: { users: include ?? false } })
   }
 
+  public async findById(id: string): Promise<Session>
+  public async findById(id: string, include: true): Promise<JoinedSession>
   public async findById(id: string, include?: boolean): Promise<Session | JoinedSession | null> {
     return await mysql.session.findUnique({ where: { id: id }, include: { users: include ?? false } })
   }
 
+  public async update(id: string, data: Prisma.SessionUpdateInput): Promise<Session>
+  public async update(id: string, data: Prisma.SessionUpdateInput, include: true): Promise<JoinedSession>
   public async update(
     id: string,
     data: Prisma.SessionUpdateInput,
--- server/src/modules/users/repositories/UserRepository.ts
+++ server/src/modules/users/repositories/UserRepository.ts
@@ -3,9 +3,15 @@ import { Prisma, Profile, User } from '@db/mysql/generated/mysql'
 export type JoinedUser = User & { profile: Profile }
 
 export default interface UserRepository {
+  save(data: Prisma.UserCreateInput): Promise<User>
+  save(data: Prisma.UserCreateInput, include: true): Promise<JoinedUser>
   save(data: Prisma.UserCreateInput, include?: boolean): Promise<User | JoinedUser>
 
+  findUserByGoogleId(googleId: string): Promise<User | null>
+  findUserByGoogleId(googleId: string, include: true): Promise<JoinedUser | null>
   findUserByGoogleId(googleId: string, include?: boolean): Promise<User | JoinedUser | null>
 
+  update(googleId: string, data: Prisma.UserUpdateInput): Promise<User>
+  update(googleId: string, data: Prisma.UserUpdateInput, include: true): Promise<JoinedUser>
   update(googleId: string, data: Prisma.UserUpdateInput, include?: boolean): Promise<User | JoinedUser>
 }
