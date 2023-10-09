44f11005a964af61869041acf726760d23ade95e
SeiwonPark <psw7347@gmail.com>
2023-10-09 16:11:57 +0900

    chore: message comment

--- server/src/configs/redis.config.ts
+++ server/src/configs/redis.config.ts
@@ -1,14 +1,13 @@
 import { createClient } from 'redis'
 
 export const redisClient = createClient({
-  legacyMode: true,
   url: 'redis://localhost:6379',
 })
 
 redisClient.connect()
 
 redisClient.on('error', (e) => {
-  console.log(`Error: ${e}`)
+  console.log(`Redis Error: ${e}`)
 })
 
 const cleanUp = async () => {
