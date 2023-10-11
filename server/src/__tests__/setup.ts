332f1374476fbfdd882c1df604bcc3f3acd2b5b8
SeiwonPark <psw7347@gmail.com>
2023-10-11 23:00:01 +0900

    fix: mock logger

--- server/src/__tests__/setup.ts
+++ server/src/__tests__/setup.ts
@@ -4,12 +4,12 @@ import { PrismaClient as MongoDBPrismaClient } from '@db/mongodb/generated/mongo
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
 import { mysql, mongodb } from '@configs/prisma.config'
 
+jest.mock('@configs/logger.config')
 jest.mock('@configs/prisma.config', () => ({
   __esModule: true,
   mysql: mockDeep<MySQLPrismaClient>(),
   mongodb: mockDeep<MongoDBPrismaClient>(),
 }))
-
 jest.mock('@configs/redis.config', () => ({
   redisClient: {
     connect: jest.fn(),
