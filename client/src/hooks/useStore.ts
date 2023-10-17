f1c053ee6b39e08f38b4c68d9674f0140161eaad
SeiwonPark <psw7347@gmail.com>
2023-10-17 19:37:36 +0900

    feat: add store

--- client/src/hooks/useStore.ts
+++ client/src/hooks/useStore.ts
@@ -7,9 +7,28 @@ interface LocalOptionState extends LocalOptions {
   setIsAudioOn: () => void
 }
 
-export const useLocalOption = create<LocalOptionState>((set) => ({
+interface AuthState {
+  isAuthenticated: boolean
+  setIsAuthenticated: (value: boolean) => void
+}
+
+/**
+ * Stores states of client's local media options.
+ */
+const useLocalOption = create<LocalOptionState>((set) => ({
   isVideoOn: false,
   isAudioOn: false,
   setIsVideoOn: () => set((state: LocalOptions) => ({ ...state, isVideoOn: !state.isVideoOn })),
   setIsAudioOn: () => set((state: LocalOptions) => ({ ...state, isAudioOn: !state.isAudioOn })),
 }))
+
+/**
+ * Stores states of client's authentication. Though it's been set by unpredictable operation, server
+ * verifies every single request with authorization process.
+ */
+const useAuthStore = create<AuthState>((set) => ({
+  isAuthenticated: false,
+  setIsAuthenticated: (value: boolean) => set((state) => ({ ...state, isAuthenticated: value })),
+}))
+
+export { useLocalOption, useAuthStore }
