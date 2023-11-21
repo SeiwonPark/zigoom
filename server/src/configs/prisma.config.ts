84ecc8157e22b919a9482380a804099fce1756da
SeiwonPark <psw7347@gmail.com>
2023-11-21 15:40:03 +0900

    fix: redundant globalizing instances

--- server/src/configs/prisma.config.ts
+++ server/src/configs/prisma.config.ts
@@ -1,12 +1,5 @@
 import { PrismaClient as MongoDBPrismaClient } from '@db/mongodb/generated/mongodb'
 import { PrismaClient as MySQLPrismaClient } from '@db/mysql/generated/mysql'
 
-const globalForPrisma = globalThis as unknown as { mysql: MySQLPrismaClient; mongodb: MongoDBPrismaClient }
-
-export const mysql = globalForPrisma.mysql || new MySQLPrismaClient()
-export const mongodb = globalForPrisma.mongodb || new MongoDBPrismaClient()
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.mysql = mysql
-  globalForPrisma.mongodb = mongodb
-}
+export const mysql = new MySQLPrismaClient()
+export const mongodb = new MongoDBPrismaClient()
