a76a60dbc8b52c2663303f206dc8af3f7d5a3928
SeiwonPark <psw7347@gmail.com>
2023-11-27 22:15:07 +0900

    fix: use local profile

--- server/scripts/authenticate.sh
+++ server/scripts/authenticate.sh
@@ -2,4 +2,4 @@
 
 source ../.env
 docker logout public.ecr.aws
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+aws ecr get-login-password --region $AWS_REGION --profile $AWS_PROFILE | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
