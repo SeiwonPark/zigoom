9dc62dd03f7800a2eed917e13c8130b9dd0a691c
SeiwonPark <psw7347@gmail.com>
2023-12-14 01:19:00 +0900

    chore: commenting out sentry just for now

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,21 +1,21 @@
-import { HttpClient } from '@sentry/integrations'
-import * as Sentry from '@sentry/react'
+// import { HttpClient } from '@sentry/integrations'
+// import * as Sentry from '@sentry/react'
 import ReactDOM from 'react-dom/client'
 
-import { VITE_SENTRY_DSN_KEY } from '@/configs/env.ts'
+// import { VITE_SENTRY_DSN_KEY } from '@/configs/env.ts'
 
 import App from './App.tsx'
 import './index.css'
 
-Sentry.init({
-  dsn: VITE_SENTRY_DSN_KEY,
-  integrations: [new Sentry.BrowserTracing(), new HttpClient()],
-  sendDefaultPii: true,
-  tracesSampleRate: 1.0,
-  // FIXME: add domain
-  tracePropagationTargets: ['localhost'],
-  debug: import.meta.env.MODE === 'development' ? true : false,
-  environment: import.meta.env.MODE,
-})
+// Sentry.init({
+//   dsn: VITE_SENTRY_DSN_KEY,
+//   integrations: [new Sentry.BrowserTracing(), new HttpClient()],
+//   sendDefaultPii: true,
+//   tracesSampleRate: 1.0,
+//   // FIXME: add domain
+//   tracePropagationTargets: ['localhost'],
+//   debug: import.meta.env.MODE === 'development' ? true : false,
+//   environment: import.meta.env.MODE,
+// })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
