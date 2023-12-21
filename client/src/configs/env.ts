8b2ad68ed14731cc83faffb865305d785871caa5
SeiwonPark <psw7347@gmail.com>
2023-12-21 18:36:29 +0900

    test: turn server

--- client/src/configs/env.ts
+++ client/src/configs/env.ts
@@ -2,6 +2,7 @@
 export const VITE_BASE_URL = import.meta.env.VITE_BASE_URL
 export const VITE_ICE_SERVER_1 = import.meta.env.VITE_ICE_SERVER_1
 export const VITE_ICE_SERVER_2 = import.meta.env.VITE_ICE_SERVER_2
+export const VITE_ICE_SERVER_3 = import.meta.env.VITE_ICE_SERVER_3
 
 /** Google */
 export const VITE_GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID
