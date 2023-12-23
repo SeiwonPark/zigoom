502224ff1af32c63ab2fdf4d9f8e7ed398ff2cf3
SeiwonPark <psw7347@gmail.com>
2023-12-24 03:53:46 +0900

    docs: add missing docstring

--- client/src/utils/check.ts
+++ client/src/utils/check.ts
@@ -4,9 +4,16 @@ interface VerifyOptions {
   params: URLSearchParams
 }
 
-export const verifySession = async ({ params }: VerifyOptions, sessionId: string | undefined): Promise<boolean> => {
-  const adhoc = params.get('adhoc')
-  const timestamp = params.get('ts')
+/**
+ * This is for verifying the session if it exists and is full (with max 9 users).
+ *
+ * @param {VerifyOptions} options - Factors to be verified.
+ * @param {string} sessionId - Entering session's id.
+ * @returns {Promise<boolean>} Result of session verification.
+ */
+export const verifySession = async (options: VerifyOptions, sessionId: string | undefined): Promise<boolean> => {
+  const adhoc = options.params.get('adhoc')
+  const timestamp = options.params.get('ts')
 
   if (!sessionId || !timestamp || !adhoc) {
     return false
