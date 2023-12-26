b48d74d5a2dd4f056866a3d1034fc380503722f9
SeiwonPark <psw7347@gmail.com>
2023-12-26 12:52:02 +0900

    refactor: guest type

--- server/src/shared/types/common.d.ts
+++ server/src/shared/types/common.d.ts
@@ -1,5 +1,9 @@
-import type { Guest } from '@shared/infra/http/middlewares/handlers'
-
 import type { TokenPayload } from 'google-auth-library'
+
+export interface Guest {
+  id: string
+  name: string
+  isGuest: boolean
+}
 
 export type Token = TokenPayload & Guest
