08b3e1a1228ff93c00e4f71c044309015879b5a3
SeiwonPark <psw7347@gmail.com>
2024-01-03 23:16:39 +0900

    refactor: auth handler

--- server/src/shared/infra/http/middlewares/handlers/auth.handler.ts
+++ server/src/shared/infra/http/middlewares/handlers/auth.handler.ts
@@ -6,52 +6,58 @@ import { ErrorCode, RequestError } from '@shared/errors'
 import { Guest } from '@shared/types/common'
 import { verifyToken } from '@utils/token'
 
-/**
- * Captures request object and parses authentication-related properties to check authentication.
- * Proceed to next middleware with user's token payload if authenticated or else guest token payload.
- */
 export const authHandler = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   logger.debug('authHandler invoked')
-  const { zigoomjwt, guestId } = req.cookies
+  const { zigoomjwt } = req.cookies
 
-  // handle when it's a guest or not signed in
   if (!zigoomjwt) {
-    logger.debug('guest mode invoked')
-    let guest: Guest
+    logger.debug('Guest mode invoked')
+    await handleGuestMode(req, res, next)
+  } else {
+    logger.debug('User mode invoked')
+    await handleUserMode(req, next, zigoomjwt)
+  }
+}
 
-    // if it's a successive request from guest
-    if (guestId) {
-      const guestData = await redisClient.get(guestId)
-      if (guestData) {
-        guest = JSON.parse(guestData)
-        req.ctx = { user: guest }
-        logger.info(`Request from guest '${guestId}'`)
-        next()
-        return
-      }
+const handleGuestMode = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  logger.debug('guest mode invoked')
+  const { guestId } = req.cookies
+  let guest: Guest
+
+  // if it's a successive request from guest
+  if (guestId) {
+    const guestData = await redisClient.get(guestId)
+    if (guestData) {
+      guest = JSON.parse(guestData)
+      req.ctx = { user: guest }
+      logger.info(`Request from guest '${guestId}'`)
+      next()
+      return
     }
-
-    // if it's a new request from guest
-    const newGuestId = randomId()
-    guest = {
-      id: newGuestId,
-      name: newGuestId,
-      isGuest: true,
-    }
-
-    req.ctx = { user: guest }
-    redisClient.set(newGuestId, JSON.stringify({ ...guest, connectedAt: Date.now() }), { EX: 3600 })
-    logger.info(`Request from guest '${newGuestId}'`)
-    res.cookie('guestId', newGuestId)
-    next()
-    return
   }
 
+  // if it's a new request from guest
+  const newGuestId = randomId()
+  guest = {
+    id: newGuestId,
+    name: newGuestId,
+    isGuest: true,
+  }
+
+  req.ctx = { user: guest }
+  redisClient.set(newGuestId, JSON.stringify({ ...guest, connectedAt: Date.now() }), { EX: 3600 })
+  logger.info(`Request from guest '${newGuestId}'`)
+  res.cookie('guestId', newGuestId)
+  next()
+  return
+}
+
+const handleUserMode = async (req: Request, next: NextFunction, token: string): Promise<void> => {
   logger.debug('user mode invoked')
   /**
    * Handles authentication logic for authenticated user.
    */
-  const payload = await verifyToken(zigoomjwt)
+  const payload = await verifyToken(token)
 
   if (!payload) {
     logger.error('Authentication required - Failed to verify token.')
